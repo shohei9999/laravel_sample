cbcb5aaa4f6a944a1641a74970e2a0cbc23e5743
shohei <s-hayashi@bitstar.jp>
2017-05-02 17:55:59 +0900

    sample

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                分類
+                分類sample
 				<select name="sel_cate1">
 				@foreach ($data as $id => $row)
 				<option value='test'>{{$row->cate1_nm}}</option>
