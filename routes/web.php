db3c03f44c8dd9c7cf08c9844e6d1215aff7726e
laravel <laravel@laravel.(none)>
2017-05-10 15:05:33 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/entry', 'EntryController@show')->name('entry');
+Route::get('/entry', 'EntryController@store')->name('entry');
