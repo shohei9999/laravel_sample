69270227abbd959f42150123b3d363b2e5170fcd
laravel <laravel@laravel.(none)>
2017-05-11 16:55:40 +0900

    DBへの登録と閲覧をできるように対応

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/entry', 'EntryController@store')->name('entry');
+Route::get('/show/{id}', 'EntryController@show')->name('show');
+Route::get('/store', 'EntryController@store')->name('store');
